dbef99b68b97d877438d09051d272b0ea09d4b9e
Guillaume <gallard.git@gmail.com>
2023-01-18 12:20:35 +0100

    v8

--- app.R
+++ app.R
@@ -22,7 +22,7 @@ ui <- fluidPage(
                         "Number of bins:",
                         min = 1,
                         max = 50,
-                        value = 15)
+                        value = 8)
         ),
 
         # Show a plot of the generated distribution
