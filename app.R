cd2fc9f0f8bdb743f2d220cae2f0fe996bb8999b
Guillaume <gallard.git@gmail.com>
2023-01-18 12:17:24 +0100

    b pink

--- app.R
+++ app.R
@@ -42,7 +42,7 @@ server <- function(input, output) {
 
         # draw the histogram with the specified number of bins
 
-        hist(x, breaks = bins, col = 'blue', border = 'blue',
+        hist(x, breaks = bins, col = 'blue', border = 'pink',
 
              xlab = 'Waiting time to next eruption (in mins)',
              main = 'Histogram of waiting times')
